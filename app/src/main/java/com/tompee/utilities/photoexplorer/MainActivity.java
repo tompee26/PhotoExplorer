c7265f2f1e612eacb720f13d2a2de0b24601d277
tompee <tompee@alink-group.co.jp>
2017-02-08 18:09:07 +0900

    Added initial list

--- app/src/main/java/com/tompee/utilities/photoexplorer/MainActivity.java
+++ app/src/main/java/com/tompee/utilities/photoexplorer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tompee.utilities.photoexplorer;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity {
+    private static final String TAG = "MainActivity";
+    private static final String PREFECTURE_ARRAY_NAME = "Prefectures";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,9 +27,7 @@ public class MainActivity extends BaseActivity {
 
         ListView listView = (ListView) findViewById(R.id.listview_category);
         List<Category> categoryList = new ArrayList<>();
-        categoryList.add(new Category("Tokyo", "http://c1.staticflickr.com/8/7022/6788055657_c450e39855_b.jpg"));
-        categoryList.add(new Category("Sapporo", "http://c2.staticflickr.com/2/1687/26375975211_2b80aa93c2_b.jpg"));
-        categoryList.add(new Category("Nagoya", "http://c1.staticflickr.com/3/2625/13011103584_c589a35c7f_b.jpg"));
+        createCategoryList(categoryList);
         CategoryListAdapter adapter = new CategoryListAdapter(this, R.layout.list_category,
                 categoryList);
         listView.setAdapter(adapter);
@@ -34,5 +36,21 @@ public class MainActivity extends BaseActivity {
     private void setToolbarTitle(int resId) {
         TextView toolbar = (TextView) findViewById(R.id.toolbar_text);
         toolbar.setText(resId);
+    }
+
+    private void createCategoryList(List<Category> list) {
+        int arrayId = getResources().getIdentifier(PREFECTURE_ARRAY_NAME, "array", getPackageName());
+        TypedArray prefectureArray = getResources().obtainTypedArray(arrayId);
+        Log.d(TAG, "Length of prefecture array: " + prefectureArray.length());
+        for (int index = 0; index < prefectureArray.length(); index++) {
+            String name = prefectureArray.getString(index);
+            Log.d(TAG, "Prefecture name: " + name);
+            int prefectureId = getResources().getIdentifier(name, "array", getPackageName());
+            TypedArray prefectureObject = getResources().obtainTypedArray(prefectureId);
+            list.add(new Category(name, prefectureObject.getString(0), prefectureObject.getString(1)));
+        }
+//        list.add(new Category("Tokyo", "http://c1.staticflickr.com/8/7022/6788055657_c450e39855_b.jpg"));
+//        list.add(new Category("Sapporo", "http://c2.staticflickr.com/2/1687/26375975211_2b80aa93c2_b.jpg"));
+//        list.add(new Category("Nagoya", "http://c1.staticflickr.com/3/2625/13011103584_c589a35c7f_b.jpg"));
     }
 }
--- app/src/main/java/com/tompee/utilities/photoexplorer/model/Category.java
+++ app/src/main/java/com/tompee/utilities/photoexplorer/model/Category.java
@@ -5,10 +5,12 @@ import android.support.annotation.NonNull;
 public class Category {
     private String mName;
     private String mUrl;
+    private String mId;
 
-    public Category(@NonNull String name, @NonNull String url) {
+    public Category(@NonNull String name, @NonNull String url, @NonNull String id) {
         mName = name;
         mUrl = url;
+        mId = id;
     }
 
     public String getName() {
@@ -17,5 +19,9 @@ public class Category {
 
     public String getUrl() {
         return mUrl;
+    }
+
+    public String getId() {
+        return mId;
     }
 }
